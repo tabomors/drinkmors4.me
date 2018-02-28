014aac695393b87562413045334ea5d65d19b17c
Stas Morozevich <s.morozevich@gpsolutions.com>
2018-02-28 20:42:44 +0300

    Add image sloth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+import bannerImg from '../assets/images/bg.jpg';
+
+const MainThing = styled.header`
+  background-image: url('${bannerImg}');
+  background-size: contain;
+  min-height: 400px;
+  opacity: 0.7;
+  background-repeat: no-repeat;
+`;
 
 const IndexPage = () => (
   <div>
     <h1>Hi there</h1>
-    <p>My name is Stas</p>
+    <p>My name is <strong>Stas Morozevich</strong></p>
     <p>
       I&#39;m passionate about programming stuff, do things with basketballs, listening to music
       right now
     </p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <MainThing />
   </div>
 );
 
