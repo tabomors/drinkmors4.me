cb22a411650dc284eb1e4489ff67393f85455081
Stas Morozevich <s.morozevich@gpsolutions.com>
2018-02-28 20:40:56 +0300

    Add styles for navbar

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+  max-width: 50%;
+  margin: auto;
+`;
 
 const NavMenu = ({ items }) => (
-  <ul>
+  <List>
     {items.map(({ name, path }) => (
       <li key={name}>
         <Link to={path}>{name}</Link>
       </li>
       ))}
-  </ul>
+  </List>
 );
 
 NavMenu.propTypes = {
