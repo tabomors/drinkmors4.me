81d33a4b572666f361261113a49aa79901040127
Stas Morozevich <s.morozevich@gpsolutions.com>
2018-02-28 20:40:10 +0300

    Add styles for logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const LogoWrapper = styled.h1`
+  margin: 0;
+`;
+
+const LogoText = styled.strong`
+  font-family: 'sans-serif';
+  line-height: 1;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  color: #000;
+  align-items: flex-end;
+
+  &:visited {
+    color: #000;
+  }
+`;
+
+const SlashSign = styled.span`
+  font-size: 2.5em;
+`;
 
 const Logo = () => (
-  <h1>
-    <Link to="/">Logo</Link>
-  </h1>
+  <LogoWrapper>
+    <StyledLink to="/">
+      <LogoText><SlashSign>/</SlashSign>home</LogoText>
+    </StyledLink>
+  </LogoWrapper>
 );
 
 export default Logo;
