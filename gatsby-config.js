b0eec4f94f4b8f4dff5600815fa03668d64874fe
Stas Morozevich <stas.morozevich@gmail.com>
2020-04-16 23:23:22 +0300

    Some tweaks for manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,9 @@ module.exports = {
       `,
       links: [],
     },
-    title: 'Stas Morozevich\' site',
-    description: 'I’m a software developer based in Brest, Belarus. Doing web, front-end, back-end.',
+    title: "Stas Morozevich' site",
+    description:
+      'I’m a software developer based in Brest, Belarus. Doing web, front-end, back-end.',
     author: '@tabomors',
   },
   plugins: [
@@ -59,13 +60,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'tabomors-site',
+        short_name: 'tabomors',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#262626',
+        theme_color: '#262626',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/me-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
